43310ce5486568228b55046742f4b27cbeb5d517
r0bulous <rrwinn@gmail.com>
2018-06-09 16:06:46 -0400

    added beginnings of UI in program class

--- VPShelter/Program.cs
+++ VPShelter/Program.cs
@@ -8,11 +8,40 @@ namespace VPShelter
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
 
+            // Define local variables for UI
 
+            //Local variables 
+            int userResponse;
+            string exitMainMenu = "no";
+
+            //Starting menu while loop
+
+            while (exitMainMenu.Equals("no"))
+            {
+                Console.WriteLine("Welcome to the pet shelter! We have the following pets today:");
+                Console.WriteLine("1: {0} 2: {1} 3: {2} ", VirtualPetShelter.petList[0], VirtualPetShelter.petList[1], VirtualPetShelter.petList[2]);
+                Console.WriteLine("Are you a manager or a volunteer? Enter 1 if you're a manager, or 2 if you're a volunteer.");
+                userResponse = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                //Logic to enter do while loop. 
+                if (userResponse == 1)
+                {
+                    Console.WriteLine("Oh, I see you're a manager.");
+                }
+                if (userResponse == 2)
+                {
+                    Console.WriteLine("Welcome! Thanks for volunteering.");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, we only support managers and volunteers right now.");
+                    break;
+                }
+            }
         }
     }
 }
--- VPShelter/VirtualPetShelter.cs
+++ VPShelter/VirtualPetShelter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace VPShelter
 {
-    class VirtualPetShelter // fields/properties for pets and employees
+    public class VirtualPetShelter // fields/properties for pets and employees
     {
 
         // Pet fields/properties
@@ -14,26 +14,38 @@ namespace VPShelter
         public int thirst { get; set; }
         public int hunger { get; set; }
         public int boredom { get; set; }
+        public List<string> PetList { get; set; } // field/property for PetList
 
-       
         // Employee fields/properties
         public string employeeName { get; set; }
         public string employeeNumber { get; set; }
+        public List<string> EmployeeList { get; set; } // field/property for EmployeeList
 
-        // Create lists of pets and employees as per project requirements
-
-        List<VirtualPet> petList = new List<VirtualPet>(); // List for pets. Unsure if this will include VirtualPet and VirtualFish classes?
-        List<Employee> employeeList = new List<Employee>(); // Employee lis. (Same q as above. Derived classes included?)
 
         // Constructors
 
         public VirtualPetShelter()
         {
-        // Default constructor
+            // Default constructor
         }
 
+        // Create lists of pets and employees as per project requirements
+
+        public static List<string> petList = new List<string>() { "Mittens", "Kimbop", "Oscar", "Lucky", }; // Updated to reflect list of types rather than objects and add pet names
+        public List<string> employeeList = new List<string>() { "Frank", "Betsy", "Pamela", "Charles", }; // Updated to reflect list of types rather than objects and add employee names
+
+        public void ShowPetList()
+        {
+        Console.WriteLine("{0}, {1}, {2}, {3},", petList[0], petList[1], petList[2], petList[3]); // test to show contents of petList
+        }
     }
-}
+
+   
+
+  }
+
+
+
 
 //The VirtualPetShelter class MUST include the appropriate instance variable(s) (fields/properties) to store the pets who reside at the shelter.
 //The VirtualPetShelter class MUST include the appropriate instance variable(s) (fields/properties) to store all of the employees who volunteer or work at the shelter.
